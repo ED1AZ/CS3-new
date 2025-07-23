a6e0c0f40e13c1cf321d25539fb197c51d771a5e
ED1AZ <ediazsilva09@gmail.com>
2025-07-23 11:21:15 -0400

    areasOfInterest function now defines & saves litter ROIs

--- bg-sub/bg-sub.py
+++ bg-sub/bg-sub.py
@@ -62,9 +62,38 @@ def bgsubtract(frame, bgsub):
     return fgmask, background_model
 
 def areasOfInterest(background, last_frame):
+    # subtract background & last_frame
     diff = cv.absdiff(background, last_frame)
-    # compare background to last frame & define specific ROI for testing
-    return diff
+
+    # filter out noise & make binary img
+    gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+    gray = cv.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv.threshold(gray, 40, 255, cv.THRESH_BINARY)[1]
+
+    contours, _ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    rois = []
+
+    for cnt in contours:
+        if cv.contourArea(cnt) > 300 or cv.contourArea(cnt) < 50:
+            # filter out big & small noise
+            continue
+        else: 
+            x, y, w, h = cv.boundingRect(cnt)
+
+            padding = 50  
+            x_new = max(x - padding, 0)
+            y_new = max(y - padding, 0)
+            x_end = min(x + w + padding, frame.shape[1])
+            y_end = min(y + h + padding, frame.shape[0])
+
+            roi = frame[y_new:y_end, x_new:x_end] 
+            rois.append(roi)
+
+    # save rois into folder
+    filename = "rois/roi"
+    for i, r in enumerate(rois):
+        cv.imwrite(filename + str(i) + ".png", r)
+        print("Saved ROI in " + filename + str(i) + ".png")
 
 def findObjects(fgmask, frame_gray):
     contours, __ = cv.findContours(fgmask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
@@ -74,6 +103,8 @@ def findObjects(fgmask, frame_gray):
 def defineObjects(contours):
     object_list = []
 
+    def match():
+        print()
     for cnt in contours:
         area = cv.contourArea(cnt)
         if area > 500:  # Filter small noise
@@ -84,10 +115,15 @@ def defineObjects(contours):
     return object_list
 
 
-cam = cv.VideoCapture("frames/home-litter.MOV")
+cam = cv.VideoCapture("frames/stationary.MOV")
+#tracker = cv.TrackerCSRT_create()
 
 
 bgsub = cv.createBackgroundSubtractorMOG2(history=20, varThreshold=50, detectShadows=True)
+ret, frame = cam.read()
+fg, initial = bgsubtract(frame, bgsub)
+initial = preprocessing(initial)
+
 """
 feature_params = dict( maxCorners = 100,
                        qualityLevel = 0.3,
@@ -105,7 +141,6 @@ old_frame = preprocessing(old_frame)
 #p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 # Create a mask image for drawing purposes
 #mask = np.zeros_like(old_frame)
-
 while cam.isOpened():
     ret, frame = cam.read()
     if not ret:
@@ -120,8 +155,9 @@ while cam.isOpened():
     num = 1
     for i in object_list:
         i.showBoundary(frame)
-        print(f"Object #{i}")
-    
+        print(f"Object #{num}")
+        num +=1
+
     """
     # calculate optical flow
     p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
@@ -147,7 +183,7 @@ while cam.isOpened():
     #threshold = cv.adaptiveThreshold(frame_gray, 0, 255, cv.THRESH_BINARY, 21, 7)
     
 
-    cv.imshow("video", frame)
+    cv.imshow("video", fgmask)
     
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
@@ -158,11 +194,49 @@ while cam.isOpened():
         current += 1
     elif frame is None:
         break
-
+        
     old_gray = frame_gray.copy()
     #p0 = good_new.reshape(-1, 1, 2)
-#diff = areasOfInterest(background, frame)
-#cv.imshow("diff", diff)
-#cv.waitKey(0)
+"""
+cv.imshow("intial", initial)
+cv.waitKey(0)
 
+#initial = cv.cvtColor(initial, cv.COLOR_BGR2GRAY)
+
+diff = areasOfInterest(initial, frame)
+gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+gray = cv.GaussianBlur(gray, (5, 5), 0)
+thresh = cv.threshold(gray, 40, 255, cv.THRESH_BINARY)[1]
+contours, _ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+rois = []
+
+for cnt in contours:
+    if cv.contourArea(cnt) > 300:
+        # filter out biggest contours
+        continue
+    elif cv.contourArea(cnt) < 50:
+        continue
+    else: 
+        x, y, w, h = cv.boundingRect(cnt)
+
+        padding = 50  
+        x_new = max(x - padding, 0)
+        y_new = max(y - padding, 0)
+        x_end = min(x + w + padding, frame.shape[1])
+        y_end = min(y + h + padding, frame.shape[0])
+
+        roi = frame[y_new:y_end, x_new:x_end] 
+        rois.append(roi)
+        # Draw the ROI on the image for visualization (optional)
+        #cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+filename = "frames/roi"
+for i, r in enumerate(rois):
+    cv.imwrite(filename + str(i) + ".png", r)
+    print("Saved ROI in " + filename + str(i) + ".png")
+"""
+#areasOfInterest(initial, frame)
+
+cv.imshow("diff", frame)
+cv.waitKey(0)
 cam.release()
