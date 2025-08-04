d946dd4ec4a0aa8ba22ccfb4b668cc5cc05ae7ad
ED1AZ <ediazsilva09@gmail.com>
2025-08-04 10:32:30 -0400

    Added iou implementation to bg-sub function, moved preprocessing file

--- bg-sub/bg-sub.py
+++ bg-sub/bg-sub.py
@@ -1,7 +1,9 @@
 import cv2 as cv
 from preprocessing import preprocessing
 import numpy as np
-orb = cv.ORB.create()
+import os
+from iou import iou
+
 # VideoObject : has features & motion, wil be tracked as it moves along the screen
 # Background Model & FGmask
 class VideoObject:
@@ -28,10 +30,10 @@ class VideoObject:
         cv.imshow("img", img_keypoints)
         cv.waitKey(0)
 
-def findObjects(fgmask):
-    contours, __ = cv.findContours(fgmask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+def findObjects(fgmask, mode=cv.RETR_EXTERNAL):
+    contours, hierarchy = cv.findContours(fgmask, mode, cv.CHAIN_APPROX_NONE)
     #cv.drawContours(frame_gray, contours, -1, (0,255,0), 3)
-    return contours
+    return contours, hierarchy
 
 
 def bgsubtract(frame, bgsub): 
@@ -52,12 +54,12 @@ def bgsubtract(frame, bgsub):
     return fgmask, background_model
 
 
-def saveROI(rois, round):
+def saveROI(rois, roundnum):
     filename = "rois/roi"
 
     for i, r in enumerate(rois):
-        cv.imwrite(filename + str(i+round) + ".png", r)
-        print("Saved ROI in " + filename + str(i+round) + ".png")
+        cv.imwrite(filename + str(i+roundnum) + ".png", r)
+        print("Saved ROI in " + filename + str(i+roundnum) + ".png")
 
 
 def findBinaryDiff(background, last_frame): 
@@ -76,15 +78,27 @@ def findBinaryDiff(background, last_frame):
     return thresh 
 
 def eliminateDuplicates(rois):
-    if not rois:
-        print("No rois found.")
-        return
-    
-    # include eliminate duplicate function
+    rois = sorted(rois, key=lambda box: (box[2]-box[0]) * (box[3]-box[1]), reverse=True)
+    precision = 5
+    if precision > 0:
+        rois = [[round(v / precision) * precision for v in box] for box in rois]
+
+    filtered = []
+
+    for i, boxA in enumerate(rois):
+        duplicate = False
+        for boxB in filtered:
+            if iou((boxA), (boxB)):
+                duplicate = True
+                break
+        if not duplicate:
+            filtered.append(boxA)
+
+    return filtered
 
 def isolateGround(background, last_frame):
     height, width, __ = background.shape
-    ground = int(height/1.5)
+    ground = int(height/2)
 
     # keep bottom part of image
     background = background[ground:height, 0:width]
@@ -92,14 +106,14 @@ def isolateGround(background, last_frame):
 
     return background, last_frame
 
-
-def areasOfInterest(background, last_frame, round):
+def areasOfInterest(background, last_frame, roundnum):
     background, last_frame = isolateGround(background, last_frame)
     
-    binary_diff = findBinaryDiff(background, last_frame)
-    static_changes = findObjects(binary_diff)
+    diff = findBinaryDiff(background, last_frame)
+    static_changes, ___ = findObjects(diff)
     rois = []
-     
+    points = []
+
     for ch in static_changes:
         area = cv.contourArea(ch)
         if area > 6000 or area < 50:
@@ -116,25 +130,31 @@ def areasOfInterest(background, last_frame, round):
             x_end = int(min(x + w + padding, last_frame.shape[1]))
             y_end = int(min(y + h + padding, last_frame.shape[0]))
 
+            """
             roi = last_frame[y_new:y_end, x_new:x_end] 
-
-            roi = cv.rectangle(roi, (x-x_new, y-y_new), (x-x_new + w, y-y_new + h), (0, 0, 255), 2)
-            
+            #roi = cv.rectangle(roi, (x-x_new, y-y_new), (x-x_new + w, y-y_new + h), (0, 0, 255), 2)
             rois.append(roi)
+            """
+            points.append([x_new, y_new, x_end, y_end])
+
+    roi_points = eliminateDuplicates(points)
+
+    for group in roi_points:
+        roi = last_frame[group[1]:group[3], group[0]:group[2]]
+        #roi = cv.rectangle(roi, (x-x_new, y-y_new), (x-x_new + w, y-y_new + h), (0, 0, 255), 2)
+        rois.append(roi)
 
     if not rois:
         print("No static changes in scene")
     else: 
-        saveROI(rois, round)
-        round += 1
+        saveROI(rois, roundnum)
+        roundnum += 1
 
     return diff
 
 def defineObjects(contours):
     object_list = []
 
-    def match():
-        print()
     for cnt in contours:
         area = cv.contourArea(cnt)
         if area > 500:  # Filter small noise (500 originally)
@@ -150,6 +170,10 @@ def getBackgroundModel(cam, bgsub):
 
     return initial
 
+# playing.mov error bc object never leaves frame
+# rolling.mov error bc bottle rolls out of frame, so its img is replaced
+# table.mov error bc bottle never detected?
+# two bystander.mov error bc WAY too many ROIS generated
 
 cam = cv.VideoCapture("frames/occlusion1.mov")
 
@@ -158,14 +182,19 @@ bgsub = cv.createBackgroundSubtractorMOG2(history=20, varThreshold=50, detectSha
 
 initial = getBackgroundModel(cam, bgsub)
 
-round = 0
+roundnum = 0
 current = 0
 avg = initial.copy()
+
+if os.path.isdir("rois"):
+    os.rmdir("rois")
+os.mkdir("rois")
 
 while cam.isOpened():
     ret, frame = cam.read()
     if not ret:
         print("Can't read frame")
+        break
     if frame is None:
         break
     else:
@@ -174,21 +203,21 @@ while cam.isOpened():
 
     #frame = cv.convertScaleAbs(frame, alpha=1, beta=-20)
 
-    video_gray = cv.cvtColor(video, cv.COLOR_BGR2GRAY)
-    fgmask, ___ = bgsubtract(video, bgsub)
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    fgmask, ___ = bgsubtract(frame_gray, bgsub)
 
     alpha = 0.2
     beta = 1 - alpha
     avg = cv.addWeighted(avg, alpha, frame, beta, 0.0)
     
-    moving_contours = findObjects(fgmask)
+    moving_contours, ___  = findObjects(fgmask)
 
     if not moving_contours:
         # moving_contours = empty list --> no objects present
         print("No objects")
 
         # update background model if no objects
-        diff = areasOfInterest(initial, avg, round)
+        diff = areasOfInterest(initial, avg, roundnum)
         
         #initial = getBackgroundModel(cam, bgsub)
 
@@ -198,16 +227,12 @@ while cam.isOpened():
         num = 1
         
         for i in object_list:
-            i.showBoundary(video)
+            i.showBoundary(frame)
             print(f"Object #{num}")
             num +=1
             print(i.area)
 
-    #prev_mask = fgmask.copy()
-    
-    #___, threshold = cv.threshold(frame, 250, 255, cv.THRESH_BINARY)
-    #threshold = cv.adaptiveThreshold(frame_gray, 0, 255, cv.THRESH_BINARY, 21, 7)
-    
+        
     #fgmask or video for viewing
     cv.imshow("video", fgmask)
     if cv.waitKey(1) & 0xFF == ord('q'):
@@ -219,21 +244,5 @@ while cam.isOpened():
         current += 1
     elif frame is None:
         break
-        
-
-# open diff file eq
-gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
-gray = cv.GaussianBlur(gray, (5, 5), 0)
-thresh = cv.threshold(gray, 40, 255, cv.THRESH_BINARY)[1]
-kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5,5))
-# open = erodes noise & dilates foreground
-thresh = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=1)
-
-# close = fills in holes within foreground
-#thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel, iterations=2)
-cv.imshow("diff", thresh)
-cv.waitKey(0)
-cv.imshow("avg", avg)
-cv.waitKey(0)
 
 cam.release() 
