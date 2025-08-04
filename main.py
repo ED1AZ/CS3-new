038aad4c07a49b914de719980c76aa9b2e01c627
ED1AZ <ediazsilva09@gmail.com>
2025-08-04 10:48:37 -0400

    added roboflow predictions to main

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-import cv2 as cv
+import roboflow as roboflow
+from dotenv import load_dotenv
+import supervision as sv
+import os
 
-cam = cv.VideoCapture(0)
-if not cam.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cam.read()
-    cv.imshow("video", frame)
+load_dotenv(dotenv_path="keys.env",)
+api_key = os.getenv("CS3_API_KEY")
 
-    if cv.waitKey(1) == ord('q'):
-        break
- 
+rf = roboflow.Roboflow(api_key=api_key)
+project = rf.workspace("cs3-ug7j5").project("litter-detection-pbz2j")
+model = project.version(6).model
+
+input_folder = "bg-sub/rois"
+output_folder = "output"
+
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith((".jpg", ".jpeg", ".png")):
+        image_path = os.path.join(input_folder, filename)
+
+        result = model.predict(image_path, confidence=70, overlap=30)
+        result_json = result.json()
+        detections = result_json["predictions"]
+
+
+        if len(detections) > 0:
+            print(f"Saved: {filename} (trash detected)")
+            for pred in detections:
+                conf = round(pred['confidence'] * 100, 2)
+                class_name = pred['class']
+                print(f" - {class_name}: {conf}%")
+
+                with open("results.txt", 'w') as f:
+                    f.write(filename + " ")
+                    f.write(str(conf))
+                    f.write("\n")
+
+            result.save(os.path.join(output_folder, filename))
+
+        else:
+            print(f"Skipped: {filename} (no trash detected)")
+
+
