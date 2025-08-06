32ecc1f935bde90447bf31ea7f5abb9ea6d5b3c2
ED1AZ <ediazsilva09@gmail.com>
2025-08-06 15:17:47 -0400

    Added ROBOFLOW roi reading to main

--- main.py
+++ main.py
@@ -1,42 +1,65 @@
 import roboflow as roboflow
 from dotenv import load_dotenv
-import supervision as sv
+from ultralytics import YOLO
 import os
+import csv
 
 load_dotenv(dotenv_path="keys.env",)
-api_key = os.getenv("CS3_API_KEY")
+api_key = os.getenv("ED1AZ_API_KEY")
 
 rf = roboflow.Roboflow(api_key=api_key)
-project = rf.workspace("cs3-ug7j5").project("litter-detection-pbz2j")
-model = project.version(6).model
+# change project & version name
+# meant to be used for yolo models 
+project = rf.workspace("ed1az").project("weights-pbmkl")
+model = project.version(2).model
 
 input_folder = "bg-sub/rois"
 output_folder = "output"
+
+# change based off of run
+modelName = "YOLOv11s"
+LitterNET_used = True
+litter_present = True
+
+video_id = str(input("Enter video id: "))
 
 for filename in os.listdir(input_folder):
     if filename.lower().endswith((".jpg", ".jpeg", ".png")):
         image_path = os.path.join(input_folder, filename)
 
-        result = model.predict(image_path, confidence=70, overlap=30)
+        result = model.predict(image_path=image_path, confidence=70, overlap=30)
         result_json = result.json()
         detections = result_json["predictions"]
 
-
+        # data format
+        # videoID, model, LitterNET_used, litter_present, litter_detected,accuracy, confidence
         if len(detections) > 0:
             print(f"Saved: {filename} (trash detected)")
             for pred in detections:
-                conf = round(pred['confidence'] * 100, 2)
+                conf = pred['confidence']
                 class_name = pred['class']
-                print(f" - {class_name}: {conf}%")
 
-                with open("results.txt", 'w') as f:
-                    f.write(filename + " ")
-                    f.write(str(conf))
-                    f.write("\n")
+                litter_detected = True
+                accuracy = 1 if litter_detected == litter_present else 0
+
+                with open("data_analysis/litternet.csv", 'a') as f:
+                    data = [video_id, modelName, LitterNET_used, filename, 0, litter_present, litter_present, conf]
+                    writer = csv.writer(f)
+                    writer.writerow(data)
+                    
 
             result.save(os.path.join(output_folder, filename))
 
         else:
             print(f"Skipped: {filename} (no trash detected)")
 
+            
+            litter_detected = False
+            accuracy = 1 if litter_detected == litter_present else 0
+            with open("results.txt", 'a') as f:
+                conf = 0
+                data = [video_id, modelName, LitterNET_used, filename, 0, litter_present, litter_present, conf]
+                writer = csv.writer(f)
+                writer.writerow(data)
+                 
 
